186943304f82c8309c201e504add3d978d1414be
Andorre777 <andorre777@gmail.com>
2025-07-18 12:15:50 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGddXrnVuz5L-9R_fQ3ab_VeWDWESh3ZU",
+  authDomain: "andorre77store.firebaseapp.com",
+  projectId: "andorre77store",
+  storageBucket: "andorre77store.firebasestorage.app",
+  messagingSenderId: "1081619107506",
+  appId: "1:1081619107506:web:5365bb5174eb72060e43ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
